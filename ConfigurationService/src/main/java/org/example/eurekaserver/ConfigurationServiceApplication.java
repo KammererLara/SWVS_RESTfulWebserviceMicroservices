f97257d7324ac9994456b3a5b4f8cd8d1a944e0a
KammererLara <lara.kammerer@gmail.com>
2024-12-07 20:54:39 +0100

    ConfigurationService fixed

--- ConfigurationService/src/main/java/org/example/eurekaserver/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/org/example/eurekaserver/ConfigurationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package org.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,12 +11,4 @@ public class ConfigurationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
 	}
-
-	//TODO: testen:
-	//	ConfigurationService starten
-	//	SensorApi starten
-	//	$ curl -X POST localhost:8080/actuator/refresh -d {} -H "Content-Type: application/json"
-	//	Port von SensorApi
-	//	ODER http://localhost:8080/message öffnen
-	//	sollte das zeigen, was unter message in a-bootiful-client.properties steht
 }
